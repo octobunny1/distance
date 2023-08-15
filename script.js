f81e8f897010244ab8dc4d4ccca81fa01858ab38
naruhina <142142981+octobunny1@users.noreply.github.com>
2023-08-15 16:36:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function checkPassword() {
+    var enteredPassword = document.getElementById("password").value.trim();
+
+    var correctPasswords = ["1potato2icecream.", "1Potato2Icecream.", "1potato2icecream", "potato", "dinesh", "naruto", "iloveyounaruto"];
+
+    if (correctPasswords.includes(enteredPassword)) {
+        window.location.href = "your_original_webpage.html"; // Replace with your original webpage URL
+    } else {
+        alert("Incorrect password. Please try again.");
+    }
+}
+
+function togglePasswordVisibility() {
+    var passwordInput = document.getElementById("password");
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+    } else {
+        passwordInput.type = "password";
+    }
+}
