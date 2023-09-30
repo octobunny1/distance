71ce6ec30f2a43dac1197000b97f66f34c2cdd7a
naruhina <142142981+octobunny1@users.noreply.github.com>
2023-09-30 22:32:02 +0530

    Add files via upload

---
+++ script_oct_version.js
@@ -0,0 +1,11 @@
+document.getElementById("showContentButton").addEventListener("click" , function() {
+    const passwordInput = document.getElementById("passwordInput");
+    const hiddenContent = document.getElementById("hiddenContent");
+    const password = ["1potato2icecream." , "1Potato2Icecream." , "1potato2icecream" , "potato" , "dinesh" , "naruto" , "iloveyounaruto"]; // Change this to your desired password
+
+    if (passwordInput.value === "potato" || passwordInput.value === "1Potato2Icecream." || passwordInput.value === "1potato2icecream." || passwordInput.value === "naruhina") {
+        hiddenContent.style.display = "block";
+    } else {
+        alert("Incorrect secret code. Try again.");
+    }
+});
