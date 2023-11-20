8e97942882357b6c795e709e12aeb711953ea0f6
naruhina <142142981+octobunny1@users.noreply.github.com>
2023-11-21 03:16:07 +0530

    Update script_oct_version.js

--- script_oct_version.js
+++ script_oct_version.js
@@ -3,7 +3,7 @@ document.getElementById("showContentButton").addEventListener("click" , function
     const hiddenContent = document.getElementById("hiddenContent");
     const password = ["1potato2icecream." , "1Potato2Icecream." , "1potato2icecream" , "potato" , "dinesh" , "naruto" , "iloveyounaruto"]; // Change this to your desired password
 
-    if (passwordInput.value === "potato" || passwordInput.value === "1Potato2Icecream.." || passwordInput.value === "1Potato2Icecream."  || passwordInput.value === "1potato2icecream." || passwordInput.value === "naruhina") {
+    if (passwordInput.value === "potato" || passwordInput.value === "1Potato2Icecream.." || passwordInput.value === "1Potato2"  || passwordInput.value === "1potato2icecream." || passwordInput.value === "naruhina") {
         hiddenContent.style.display = "block";
     } 
 });
